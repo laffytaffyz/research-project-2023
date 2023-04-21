0c8eda5529c78c5de640f6508f4a958775c74fe2
laffytaffyz <74117435+laffytaffyz@users.noreply.github.com>
2023-04-21 11:45:25 -0400

    Update mag vs color based on age.py

--- mag vs color based on age.py
+++ mag vs color based on age.py
@@ -4,19 +4,23 @@ import numpy as np
 import statsmodels.formula.api as smf
 from sklearn.linear_model import LinearRegression
 
+# processing data and removing outliers
 df = pd.read_csv("C:\\Users\\tiffa\\Downloads\\2022-2023 HSR\\i-band red clump.csv", encoding='latin-1')
 df = df.drop(df[df['m_i']-df['m-M']-1.5424*df['reddening'] > 4].index)
 mag = np.array(df['m_i']-df['m-M']-1.5424*df['reddening'])
 color = np.array(df['m_v'] - df['m_i'] - 1.60*df['reddening'])
 
+# calculating properties of young red clump
 youngdf = df[df['age'] < 2000]
 youngmag = np.array(youngdf['m_i']-youngdf['m-M']-1.5424*youngdf['reddening'])
 youngcolor = np.array(youngdf['m_v'] - youngdf['m_i'] - 1.60*youngdf['reddening'])
 
+# calculating properties of old red clump
 olddf = df[df['age'] > 2000]
 oldmag = np.array(olddf['m_i']-olddf['m-M']-1.5424*olddf['reddening'])
 oldcolor = np.array(olddf['m_v'] - olddf['m_i'] - 1.60*olddf['reddening'])
 
+# defining y and x values for statistical OLS analysis
 youngx = youngcolor
 oldx = oldcolor
 youngy = youngmag
@@ -37,6 +41,7 @@ ax.set_xlabel(r'$(V-I)_0$ (mag)')
 ax.scatter(youngx,youngy,s=10,c="blue") 
 ax.scatter(oldx,oldy,s=10,c="red") 
 
+# drawing fit lines
 youngline = np.linspace(youngx.min(), youngx.max(), 100) 
 youngmodel = np.poly1d(np.polyfit(youngx, youngy, 1)) 
 ax.plot(youngline, youngmodel(youngline), c='blue',linewidth=1.0)
@@ -49,7 +54,4 @@ line = np.linspace(color.min(), color.max(), 100)
 model = np.poly1d(np.polyfit(color, mag, 1)) 
 ax.plot(line, model(line), c='black',linewidth=1.5)
 
-# ax.text(0.68, -1.85, r"$M_{K,young} = (0.1374 \pm 0.361)*(J-K)_0 + (-1.6926 \pm 0.0.233)$", fontsize = 8)
-# ax.text(0.7, -1.81, r"$M_{K,old} = (-1.2298 \pm 1.868)*(J-K)_0 + (-0.7757 \pm 1.121)$",  fontsize = 8)
-# regular mk is (-0.0249 +- 0.348)*color + (-1.5647 +- 0.219)
 plt.show()
