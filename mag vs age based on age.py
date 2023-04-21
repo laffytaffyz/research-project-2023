07601d0479d2117faad34dcdf8d0dd0fa789be70
laffytaffyz <74117435+laffytaffyz@users.noreply.github.com>
2023-04-21 11:48:09 -0400

    Update mag vs age based on age.py

--- mag vs age based on age.py
+++ mag vs age based on age.py
@@ -5,19 +5,23 @@ import statsmodels.formula.api as smf
 from sklearn.linear_model import LinearRegression
 import math
 
+# processing data and removing outliers
 df = pd.read_csv("C:\\Users\\tiffa\\Downloads\\2022-2023 HSR\\i-band red clump.csv", encoding='latin-1')
 df = df.drop(df[df['m_i']-df['m-M']-1.5424*df['reddening'] > 4].index)
 mag = df['m_i']-df['m-M']-1.5424*df['reddening']
 age = np.log10(np.array((10**6)*df['age']))
 
+# calculating properties of young red clump
 youngdf = df[df['age'] < 2000]
 youngmag = np.array(youngdf['m_i']-youngdf['m-M']-1.5424*youngdf['reddening'])
 youngage = np.log10(np.array((10**6)*youngdf['age']))
 
+# calculating properties of old red clump
 olddf = df[df['age'] > 2000]
 oldmag = np.array(olddf['m_i']-olddf['m-M']-1.5424*olddf['reddening'])
 oldage = np.log10(np.array((10**6)*olddf['age']))
 
+# defining y and x values for statistical OLS analysis
 youngx = youngage
 oldx = oldage
 youngy = youngmag
@@ -38,6 +42,7 @@ ax.set_xlabel('Log Age (dex)')
 ax.scatter(youngx,youngy,s=10,c="blue") 
 ax.scatter(oldx,oldy,s=10,c="red") 
 
+# drawing fit lines
 youngline = np.linspace(youngx.min(), youngx.max(), 100) 
 youngmodel = np.poly1d(np.polyfit(youngx, youngy, 1)) 
 ax.plot(youngline, youngmodel(youngline), c='blue',linewidth=1.0)
