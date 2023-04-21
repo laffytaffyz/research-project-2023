3e15181c9288b49444fee5b1ad2db9c43fdc13cc
laffytaffyz <74117435+laffytaffyz@users.noreply.github.com>
2023-04-21 11:50:45 -0400

    Update and rename mag vs feh based on age.py to i-band mag vs feh based on age.py

--- i-band mag vs feh based on age.py
+++ i-band mag vs feh based on age.py
@@ -4,19 +4,23 @@ import numpy as np
 import statsmodels.formula.api as smf
 from sklearn.linear_model import LinearRegression
 
+# processing data and removing outliers
 df = pd.read_csv("C:\\Users\\tiffa\\Downloads\\2022-2023 HSR\\i-band red clump.csv", encoding='latin-1')
 df = df.drop(df[df['m_i']-df['m-M']-1.5424*df['reddening'] > 4].index)
 mag = np.array(df['m_i']-df['m-M']-1.5424*df['reddening'])
 feh = np.array(df['FeH'])
 
+# calculating properties of young red clump
 youngdf = df[df['age'] < 2000]
 youngmag = np.array(youngdf['m_i']-youngdf['m-M']-1.5424*youngdf['reddening'])
 youngfeh = np.array(youngdf['FeH'])
 
+# calculating properties of old red clump
 olddf = df[df['age'] > 2000]
 oldmag = np.array(olddf['m_i']-olddf['m-M']-1.5424*olddf['reddening'])
 oldfeh = np.array(olddf['FeH'])
 
+# defining y and x values for statistical OLS analysis
 youngx = youngfeh
 oldx = oldfeh
 youngy = youngmag
@@ -37,6 +41,7 @@ ax.set_xlabel(r'$[Fe/H]$ (dex)')
 ax.scatter(youngx,youngy,s=10,c="blue") 
 ax.scatter(oldx,oldy,s=10,c="red") 
 
+# drawing regression lines
 youngline = np.linspace(youngx.min(), youngx.max(), 100) 
 youngmodel = np.poly1d(np.polyfit(youngx, youngy, 1)) 
 ax.plot(youngline, youngmodel(youngline), c='blue',linewidth=1.0)
@@ -48,9 +53,5 @@ ax.plot(oldline, oldmodel(oldline), c='red',linewidth=1.0)
 line = np.linspace(feh.min(), feh.max(), 100) 
 model = np.poly1d(np.polyfit(feh, mag, 1)) 
 ax.plot(line, model(line), c='black',linewidth=1.5)
-
-# ax.text(-1.1, -1.85, r"$M_{K,young} = (0.0992 \pm 0.474)*[Fe/H] + (-1.5892 \pm 0.091)$", fontsize = 8)
-# ax.text(-1.1, -1.81, r"$M_{K,old} = (-0.2254 \pm 0.150)*[Fe/H] + (-1.6033 \pm 0.075)$",  fontsize = 8)
-# regular is mk = (-0.2424 +- 0.143)feh + (-1.6421 +- 0.049)
 
 plt.show()
