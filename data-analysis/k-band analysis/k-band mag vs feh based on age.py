074faa817f16ee05b70689060fbc689f56aa6a56
laffytaffyz <74117435+laffytaffyz@users.noreply.github.com>
2023-04-22 08:00:59 -0400

    Update and rename mag vs feh based on age.py to data-analysis/k-band analysis/k-band mag vs feh based on age.py

--- data-analysis/k-band analysis/k-band mag vs feh based on age.py
+++ data-analysis/k-band analysis/k-band mag vs feh based on age.py
@@ -4,21 +4,23 @@ import numpy as np
 import statsmodels.formula.api as smf
 from sklearn.linear_model import LinearRegression
 
+# processing data
 df = pd.read_csv("C:\\Users\\tiffa\\Downloads\\2022-2023 HSR\\red clump.csv")
 df = df.dropna()
-print(df)
-
 mag = np.array(df['MK'])
 feh = np.array(df['FeH'])
 
+# calculating properties of young red clump
 youngdf = df[df['Age'] < 2000]
 youngmag = np.array(youngdf['MK'])
 youngfeh = np.array(youngdf['FeH'])
 
+# calculating properties of old red clump
 olddf = df[df['Age'] > 2000]
 oldmag = np.array(olddf['MK'])
 oldfeh = np.array(olddf['FeH'])
 
+# defining y and x axis for statistical OLS analysis
 youngx = youngfeh
 oldx = oldfeh
 youngy = youngmag
@@ -39,6 +41,7 @@ ax.set_xlabel(r'$[Fe/H]$ (dex)')
 ax.scatter(youngx,youngy,s=10,c="blue") 
 ax.scatter(oldx,oldy,s=10,c="red") 
 
+# drawing fit lines
 youngline = np.linspace(youngx.min(), youngx.max(), 100) 
 youngmodel = np.poly1d(np.polyfit(youngx, youngy, 1)) 
 ax.plot(youngline, youngmodel(youngline), c='blue',linewidth=1.0)
@@ -50,9 +53,5 @@ ax.plot(oldline, oldmodel(oldline), c='red',linewidth=1.0)
 line = np.linspace(feh.min(), feh.max(), 100) 
 model = np.poly1d(np.polyfit(feh, mag, 1)) 
 ax.plot(line, model(line), c='black',linewidth=1.5)
-
-# ax.text(-1.1, -1.85, r"$M_{K,young} = (0.0992 \pm 0.474)*[Fe/H] + (-1.5892 \pm 0.091)$", fontsize = 8)
-# ax.text(-1.1, -1.81, r"$M_{K,old} = (-0.2254 \pm 0.150)*[Fe/H] + (-1.6033 \pm 0.075)$",  fontsize = 8)
-# regular is mk = (-0.2424 +- 0.143)feh + (-1.6421 +- 0.049)
 
 plt.show()
