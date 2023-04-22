0f89db6c8593e2e1218a363df24c22d7b70a6a7d
laffytaffyz <74117435+laffytaffyz@users.noreply.github.com>
2023-04-22 08:24:29 -0400

    Update and rename oneregression.py to data-analysis/metallicity vs age plot.py

--- data-analysis/metallicity vs age plot.py
+++ data-analysis/metallicity vs age plot.py
@@ -4,12 +4,14 @@ import numpy as np
 import statsmodels.formula.api as smf
 from sklearn.linear_model import LinearRegression
 
+# data processing
 df = pd.read_csv("C:\\Users\\tiffa\\Downloads\\2022-2023 HSR\\red clump.csv")
 mag = np.array(df['MK'])
 color = np.array(df['J-K'])
 age = np.log10(np.array((10**6)*df['Age']))
 feh = np.array(df['FeH'])
 
+# defining y and x values for statistical OLS analysis 
 x = age
 y = feh
 analysis = smf.ols(formula = 'y ~ x', data = df).fit() 
@@ -22,6 +24,7 @@ plt.xlabel(r'$\log(Age)$ (dex)')
 plt.ylabel(r'$[Fe/H]$ (dex)') 
 plt.scatter(x,y) 
 
+# drawing fit lines
 line = np.linspace(x.min(), x.max(), 100) 
 model = np.poly1d(np.polyfit(x, y, 1)) 
 plt.plot(line, model(line), c='black',linewidth=1.0)
